94afb80736b256a2ca66a2cf129d42bf408e3954
Andre <sebastianvh86@gmail.com>
2025-12-15 13:17:57 -0600

    first commit

---
+++ src/main/java/org/sebas/cloudp/CloudPApplication.java
@@ -0,0 +1,13 @@
+package org.sebas.cloudp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudPApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sebas/cloudp/CloudPApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sebas.cloudp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudPApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
